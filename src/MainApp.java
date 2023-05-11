f8d0a32aa3d865c0e18a09b2c45ec7c6dcb0a5b3
ReinaSparklechu <Sparklechus@gmail.com>
2023-05-11 12:33:21 +0800

    Library app configured with spring xml

--- src/MainApp.java
+++ src/MainApp.java
@@ -12,17 +12,11 @@ public class MainApp {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         Library library = applicationContext.getBean("library", Library.class);
         System.out.println(library);
-        library.addBook(new Book("1234-1-12-12345-6", ".NET book 1",".NET"));
-        library.addBook(new Book("1234-1-12-12345-6", ".NET book 2",".NET")); // should not be added as same isbn and equal obj
-        library.addBook(new Book("1234-1-12-12345-1", "Head First Java", "Java"));
-        library.addBook(new Book("1234-1-12-12345-2", "Core Java", "Java"));
-        library.addBook(new Book("1234-1-12-12345-3", "Automatic the Boring Stuff with Python", "Python"));
-        library.addBook(new Book("1234-1-12-12345-4", "Python Crash Course", "Python"));
-        Compartment compartment = applicationContext.getBean(".NETCompartment", Compartment.class);
-        System.out.println(compartment); // note compartment used in library is the same bean that was returned
-        System.out.println(library); // books added into the library
-        Library library1 = applicationContext.getBean("library", Library.class);
-        System.out.println(library1); // note its the same library
+//        Compartment compartment = applicationContext.getBean(".NETCompartment", Compartment.class);
+//        System.out.println(compartment); // note compartment used in library is the same bean that was returned
+//        System.out.println(library); // books added into the library
+//        Library library1 = applicationContext.getBean("library", Library.class);
+//        System.out.println(library1); // note its the same library
         System.exit(0);
     }
 }
